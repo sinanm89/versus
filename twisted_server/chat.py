9644060938a026fa9654e224a7cf8f58e4a57af2
sinanm89 <sinanm89@gmail.com>
2014-07-16 06:08:10 +0300

    first 2 phases of the game are done

--- twisted_server/chat.py
+++ twisted_server/chat.py
@@ -1,41 +1,83 @@
+import json
 from twisted.internet.protocol import Factory
 from twisted.protocols.basic import LineReceiver
 from twisted.internet import reactor
+import exceptions
+
 
 class Chat(LineReceiver):
 
     def __init__(self, users):
-        self.users = users
         self.name = None
-        self.state = "GETNAME"
+        self.users = users
+        self.is_ready = False
+        self.max_players = 2
+        self.state = "WAIT_PLAYERS"
+
+    def __unicode__(self):
+        return "%s's Chat instance " % self.name
 
     def connectionMade(self):
-        self.sendLine("What's your name?")
+        print 'Our current capacity is %s/%s' %(str(len(self.users)+1), self.max_players)
+        self.handle_GETNAME()
+        self.check_phase()
 
     def connectionLost(self, reason):
         if self.name in self.users:
             del self.users[self.name]
 
     def lineReceived(self, line):
-        if self.state == "GETNAME":
-            self.handle_GETNAME(line)
+        # import ipdb;ipdb.set_trace()
+        try:
+            data = json.loads(line)
+            print data
+        except exceptions.ValueError:
+            data = None
+            #TODO: logger here
+        if data:
+            print 'we have data, we need logic here'
+            if self.state == "READY_PLAYERS":
+                self.is_ready = data.get('is_ready', False)
+            self.check_phase()
+            #TODO: Parse json data to logic
+            # self.broadcast_fphase_change(line)
+
+    def handle_GETNAME(self):
+        self.name = len(self.users)+1
+        self.users[str(self.name)] = self
+        self.sendLine("joined")
+
+    def check_phase(self):
+        if self.state == "WAIT_PLAYERS":
+            self.current_player_count = len(self.users)
+            if self.current_player_count == self.max_players:
+                self.broadcast_phase_change("READY_PLAYERS")
+        elif self.state == "READY_PLAYERS":
+            if self.users_are_ready():
+                self.broadcast_phase_change("START_GAME")
+            else:
+                #TODO: AT THIS POINT THE READY BUTTON FLASHES IN GAME
+                pass
+        elif self.state == "START_GAME":
+            pass
+        elif self.state == "INGAME":
+            pass
+        elif self.state == "ENDGAME":
+            pass
         else:
-            self.handle_CHAT(line)
+            pass
 
-    def handle_GETNAME(self, name):
-        if name in self.users:
-            self.sendLine("Name taken, please choose another.")
-            return
-        self.sendLine("Welcome, %s!" % (name,))
-        self.name = name
-        self.users[name] = self
-        self.state = "CHAT"
 
-    def handle_CHAT(self, message):
-        message = "<%s> %s" % (self.name, message)
-        for name, protocol in self.users.iteritems():
-            if protocol != self:
-                protocol.sendLine(message)
+    def broadcast_phase_change(self, phase):
+        self.state = phase
+        for users, protocol in self.users.iteritems():
+            protocol.sendLine('{ "PHASE_CHANGE" : "%s" }' % phase)
+
+    def users_are_ready(self):
+        for user, protocol in self.users.iteritems():
+            if not protocol.is_ready:
+                return False
+        return True
 
 
 class ChatFactory(Factory):
